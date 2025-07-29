054f37b19574ce62cfe2ff922f2fa4b3a12eff86
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-07-30 03:18:24 +0500

    v nice transition

---
+++ src/components/PageTransitionWrapper.tsx
@@ -0,0 +1,36 @@
+// layout.tsx or page wrapper component
+"use client";
+import { usePathname } from "next/navigation";
+import { AnimatePresence, motion } from "framer-motion";
+import { ReactNode } from "react";
+
+export default function PageTransitionWrapper({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const pathname = usePathname();
+
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={pathname}
+        initial={{ opacity: 0, scale: 0.98 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.98 }}
+        transition={{ duration: 0.25, ease: [0.4, 0, 0.2, 1] }} // smooth bezier
+      >
+        {children}
+      </motion.div>
+      {/* <motion.div
+        key={pathname}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.2 }}
+      >
+        {children}
+      </motion.div> */}
+    </AnimatePresence>
+  );
+}
