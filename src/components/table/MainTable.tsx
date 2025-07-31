3d7d3e15f05f814aca8d3ec0d383f54143867474
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-08-01 00:46:17 +0500

    paddign change test

--- src/components/table/MainTable.tsx
+++ src/components/table/MainTable.tsx
@@ -23,7 +23,7 @@ export default function MainTable() {
           </div>
         </div>
 
-        <div className="flex justify-end p-4">
+        <div className="flex justify-end p-2">
           <ExpenseModalController />
         </div>
 
