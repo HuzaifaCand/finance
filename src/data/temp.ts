e4f209aa01c52f55210e846c97cfa0b1557e6b2c
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-08-04 14:46:46 +0500

    empty table chk

--- src/data/temp.ts
+++ src/data/temp.ts
@@ -19,29 +19,7 @@ export const baseCategories = [
   "Miscellaneous",
 ];
 
-export const tempItems: Items[] = [
-  { name: "Biryani - was mid", category: "Food", method: "Cash", cost: 14.0 },
-  {
-    name: "MTR to Central",
-    category: "Transport",
-    method: "Octopus",
-    cost: 14.5,
-  },
-  { name: "Milk Tea", category: "Beverage", method: "Cash", cost: 12.0 },
-
-  {
-    name: "Bus to Campus",
-    category: "Transport",
-    method: "Octopus",
-    cost: 6.8,
-  },
-  { name: "Lunch", category: "Food", method: "Cash", cost: 12.0 },
-  { name: "Notebook", category: "Education", method: "Credit", cost: 13.0 },
-  { name: "Dinner", category: "Food", method: "Credit", cost: 78.5 },
-
-  { name: "Water Bottle", category: "Beverage", method: "Cash", cost: 10.0 },
-  { name: "Printing", category: "Education", method: "Octopus", cost: 5.0 },
-];
+export const tempItems: Items[] = [];
 
 export const totalCost = tempItems.reduce((sum, item) => sum + item.cost, 0);
 
