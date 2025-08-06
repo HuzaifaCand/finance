38b85d6d6b750b87ee11c0211ee5b129212b348c
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-08-06 21:06:21 +0500

    scnn

--- src/components/table/DateHead.tsx
+++ src/components/table/DateHead.tsx
@@ -20,31 +20,11 @@ export default function DateHead({ date, setDate }: Props) {
 
   return (
     <div className="flex flex-col sm:flex-row sm:items-center gap-1 sm:gap-3">
-      <Popover className="relative sm:hidden mb-1">
-        <PopoverButton className="p-1.5 rounded-md bg-secondary/50 text-moreWhite border border-muted/30 hover:bg-secondary/70">
-          <CalendarIcon className="w-3 h-3 sm:w-4 sm:h-4" />
-        </PopoverButton>
-
-        <PopoverPanel className="fixed inset-0 flex items-start justify-center sm:absolute sm:inset-auto sm:mt-2 z-50">
-          {({ close }) => (
-            <div className="bg-background p-4 rounded-lg shadow-lg w-64 max-h-[80vh] overflow-y-auto">
-              <Calendar
-                selected={date}
-                onSelect={(d) => {
-                  setDate(d);
-
-                  close(); // auto-close popover
-                }}
-              />
-            </div>
-          )}
-        </PopoverPanel>
-      </Popover>
       <h1 className="text-md [@media(min-width:400px)]:text-lg [@media(min-width:470px)]:text-xl sm:text-2xl md:text-3xl font-bold text-moreWhite tracking-tight">
         {format(date, "EEEE d MMMM")}
       </h1>
 
-      <Popover className="relative hidden sm:inline mt-1">
+      <Popover className="relative mt-1">
         <PopoverButton className="p-1.5 rounded-md bg-secondary/50 text-moreWhite border border-muted/30 hover:bg-secondary/70">
           <CalendarIcon className="w-3 h-3 sm:w-4 sm:h-4" />
         </PopoverButton>
