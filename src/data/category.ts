90702323af43882e6ad70a95add48a2fe692ee6e
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-08-07 04:38:24 +0500

    forgot to add this fella

---
+++ src/data/category.ts
@@ -0,0 +1,13 @@
+export const baseCategories = [
+  "Food",
+  "Groceries",
+  "Eating Out",
+  "Transport",
+  "Utilities",
+  "Fitness",
+  "Entertainment",
+  "Health",
+  "Education",
+  "Shopping",
+  "Miscellaneous",
+];
