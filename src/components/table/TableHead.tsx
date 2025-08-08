fee7535337ca3bd0d2ded342f31f80c749c65534
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-08-08 15:25:53 +0500

    more responsiveness scn

--- src/components/table/TableHead.tsx
+++ src/components/table/TableHead.tsx
@@ -2,19 +2,13 @@ export default function TableHead() {
   return (
     <thead className="bg-secondary/60 text-moreWhite text-[10px] sm:text-xs">
       <tr>
-        <th className="py-4 px-2 [@media(min-width:460px)]:px-3 [@media(min-width:740px)]:px-4 text-left font-semibold rounded-tl-xl">
+        <th className="py-4 px-3 sm:px-4 text-left font-semibold rounded-tl-xl">
           Desc
         </th>
-        <th className="py-4 px-2 [@media(min-width:460px)]:px-3 [@media(min-width:740px)]:px-4 text-left font-semibold ">
-          Category
-        </th>
-        <th className="py-4 px-2 [@media(min-width:460px)]:px-3 [@media(min-width:740px)]:px-4 text-left font-semibold ">
-          Method
-        </th>
-        <th className="py-4 px-2 [@media(min-width:460px)]:px-3 [@media(min-width:740px)]:px-4 text-left font-semibold">
-          Cost
-        </th>
-        <th className="py-4 px-2 [@media(min-width:460px)]:px-3 [@media(min-width:740px)]:px-4 rounded-tr-xl"></th>
+        <th className="py-4 px-3 sm:px-4 text-left font-semibold ">Category</th>
+        <th className="py-4 px-3 sm:px-4 text-left font-semibold ">Method</th>
+        <th className="py-4 px-3 sm:px-4 text-left font-semibold">Cost</th>
+        <th className="py-4 px-3 sm:px-4 rounded-tr-xl"></th>
       </tr>
     </thead>
   );
