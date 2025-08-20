8a3f5150a6bbfb04c1762ae1e646de1f7de0a020
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-08-20 23:49:26 +0500

    login page done

--- src/app/(app)/about/page.tsx
+++ src/app/(app)/about/page.tsx
@@ -10,20 +10,24 @@ export default function AboutPage() {
         {/* Sections */}
         <div className="space-y-4 text-sm leading-relaxed">
           <section>
-            <h2 className="font-semibold text-accent mb-1">1. Data Security</h2>
+            <h2 className="font-semibold text-accent mb-1">FinanceHK</h2>
             <p className="text-muted">
-              All of your data is securely stored and protected.
+              FinanceHK is a lightweight, fast, efficient, finance tracking app
+              built for students in Hong Kong. It is a mobile-first but
+              keyboard-optimized expense tracker
             </p>
           </section>
 
           <section>
-            <h2 className="font-semibold text-accent mb-1">
-              2. Personal Access
-            </h2>
+            <h2 className="font-semibold text-accent mb-1">Current Features</h2>
             <p className="text-muted">
               Only you have access to your data. No viewing or monitoring is
               done to your information.
             </p>
+          </section>
+          <section>
+            <h2 className="font-semibold text-accent mb-1">Future Features</h2>
+            <p className="text-muted">The ability to export your data</p>
           </section>
         </div>
 
--- src/components/LoginComponent.tsx
+++ src/components/LoginComponent.tsx
@@ -1,4 +1,12 @@
-import Image from "next/image";
+import localFont from "next/font/local";
+
+const aquire = localFont({
+  src: [
+    {
+      path: "../../public/fonts/Aquire.otf",
+    },
+  ],
+});
 
 export default function LoginComponent({
   handleGoogleSignIn,
@@ -18,9 +26,20 @@ export default function LoginComponent({
     );
   return (
     <div className="flex flex-col gap-2 items-center justify-center h-screen bg-background">
+      <div className="text-center mb-4">
+        <div className={aquire.className}>
+          <h1 className="text-4xl sm:text-5xl text-moreWhite mb-3">
+            FINANCE<span className="text-teal">HK</span>
+          </h1>
+        </div>
+        <p className="text-muted max-w-[300] text-xs sm:text-sm">
+          Lightweight, fast, efficient finance tracker for students in Hong
+          Kong.{" "}
+        </p>
+      </div>
       <button
         onClick={handleGoogleSignIn}
-        className="py-3 px-8 sm:px-12 sm:py-4 rounded-lg bg-secondary text-moreWhite hover:bg-secondary/80 text-md sm:text-lg transition duration-200 font-semibold"
+        className="py-3.5 px-16 sm:px-24 rounded-lg bg-secondary text-moreWhite hover:bg-secondary/80 text-sm sm:text-md  transition duration-200 font-semibold"
       >
         Sign in with Google
       </button>
