58f8bc0558a68e245292e864adbd463af7f1a670
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-10-21 21:36:37 +0500

    improved

--- src/components/LoginComponent.tsx
+++ src/components/LoginComponent.tsx
@@ -1,4 +1,5 @@
 import localFont from "next/font/local";
+import Loading from "./Loading";
 
 const aquire = localFont({
   src: [
@@ -15,15 +16,7 @@ export default function LoginComponent({
   handleGoogleSignIn: () => void;
   loading: boolean;
 }) {
-  if (loading)
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <div className="flex items-center gap-2 text-teal text-md sm:text-lg font-medium">
-          <span className="h-5 w-5 rounded-full border-2 border-teal border-t-transparent animate-spin" />
-          <span>Routing...</span>
-        </div>
-      </div>
-    );
+  if (loading) return <Loading text="Routing" />;
   return (
     <div className="flex flex-col gap-2 items-center justify-center h-screen bg-background">
       <div className="text-center mb-4">
